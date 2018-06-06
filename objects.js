f33792755cf601b723a5e84ab7f9424accc4e57d
Alejandra Castillo <ahle.castillo@gmail.com>
2018-06-06 01:32:44 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = new Object ({keys: '0'});
+
+function updatePlaylist (playlist, artistName,songTitle) {
+playlist[artistName] = sogTitle 
+return playlist
+  
+}
+
+function removeFromPlaylist (playlist, artistName, songTitle) { 
+  delete playlist[artistName] 
+  return plalist
+}
+
